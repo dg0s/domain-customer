0e1e20662403faf72dd104a459515904c01e0bdd
Bill Bensing <billbensing@wb3tech.com>
2019-09-09 21:08:59 -0400

    made CustomerEntity an abstract class, pulled logic from Customer and put it into Customer Entit

--- src/main/java/com/wb3/hexarch/domain/customer/entity/Customer.java
+++ src/main/java/com/wb3/hexarch/domain/customer/entity/Customer.java
@@ -1,46 +1,18 @@
 package com.wb3.hexarch.domain.customer.entity;
 
+
 import com.wb3.hexarch.domain.Identity;
 import com.wb3.hexarch.domain.customer.boundary.CustomerRequest;
 
-import java.util.UUID;
-
-public class Customer implements CustomerEntity {
-
-    private Identity id;
-    private String firstName;
-    private String lastName;
+public class Customer extends CustomerEntity {
 
     public Customer(String firstName, String lastName) {
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
-        this.id = new Identity();
+        super(firstName, lastName);
     }
 
     public Customer(CustomerRequest request) {
-        this.id = new Identity(request.getId());
-        this.firstName = request.getFirstName();
-        this.lastName = request.getLastName();
-    }
-
-    private void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    private void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public UUID getId() {
-        return this.id.get();
-    }
-
-    public String getFirstName() {
-        return this.lastName;
-    }
-
-    public String getLastName() {
-        return this.firstName;
+        super(request.getFirstName(), request.getLastName());
+        this.setId(new Identity(request.getId()));
     }
 
 }
--- src/main/java/com/wb3/hexarch/domain/customer/entity/CustomerEntity.java
+++ src/main/java/com/wb3/hexarch/domain/customer/entity/CustomerEntity.java
@@ -1,9 +1,42 @@
 package com.wb3.hexarch.domain.customer.entity;
 
+import com.wb3.hexarch.domain.Identity;
+
 import java.util.UUID;
 
-public interface CustomerEntity {
-    UUID getId();
-    String getFirstName();
-    String getLastName();
+public abstract class CustomerEntity {
+
+    private Identity id;
+    private String firstName;
+    private String lastName;
+
+    public CustomerEntity(String firstName, String lastName) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.id = new Identity();
+    }
+
+    protected void setId(Identity id) {
+        this.id = id;
+    }
+
+    protected void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    protected void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public UUID getId() {
+        return this.id.get();
+    }
+
+    public String getFirstName() {
+        return this.lastName;
+    }
+
+    public String getLastName() {
+        return this.firstName;
+    }
 }
