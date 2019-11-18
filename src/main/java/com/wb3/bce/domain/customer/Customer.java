799e1dbe8121257c4a5f491f9c69c1550566b7b5
Bill Bensing <billbensing@wb3tech.com>
2019-11-18 16:01:44 -0500

    Made customer package-private

--- src/main/java/com/wb3/bce/domain/customer/Customer.java
+++ src/main/java/com/wb3/bce/domain/customer/Customer.java
@@ -3,13 +3,13 @@ package com.wb3.bce.domain.customer;
 
 import com.wb3.bce.domain.kernal.Identity;
 
-public class Customer extends CustomerEntity {
+class Customer extends CustomerEntity {
 
-    public Customer(String firstName, String lastName) {
+    Customer(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
-    public Customer(CustomerRequest request) {
+    Customer(CustomerRequest request) {
         super(request.getFirstName(), request.getLastName());
         this.setId(Identity.Of(request.getId()));
     }
